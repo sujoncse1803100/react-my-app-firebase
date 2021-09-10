5fd046a4195320e4ab279d763f20888bbc0d7a2f
Sujon Ali <sujon.cse.1803100@gmail.com>
2021-09-10 12:00:02 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import firebaseConfig from './FirebaseConfig';
+import { getAuth, signOut, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from 'react';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    displayName: '',
+    email: '',
+    photoURL: '',
+    isLoggedIn: false,
+  })
+  const provider = new GoogleAuthProvider();
+
+  const handleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        const signInUser = {
+          displayName: user.displayName,
+          isLoggedIn: true,
+          email: user.email,
+          photoURL: user.photoURL
+        }
+        setUser(signInUser);
+      }).catch(error => {
+      })
+  }
+
+  const handleSignOut = (signInUser) => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        const signOutUser = {
+          displayName: '',
+          email: '',
+          photoURL: '',
+          isLoggedIn: false,
+        }
+        setUser(signOutUser);
+      }).catch((error) => {
+
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {
+          user.isLoggedIn &&
+          <div className="mt-3">
+            <img style={{ width: '100px', borderRadius: '50%' }} src={user.photoURL} alt="" />
+            <h3 className="mb-1 mt-1" style={{ fontStyle: 'italic' }} >Welcome, Mr. {user.displayName}</h3>
+            <h4>Email : {user.email}</h4>
+          </div>
+        }
+      </div>
+      <div>
+        {
+          user.isLoggedIn ? <button onClick={handleSignOut} className="btn mt-5 mb-5 btn-primary">Sign out</button> :
+            <button onClick={handleSignIn} className="btn mt-5 mb-5 btn-primary">Sign In</button>
+        }
+      </div>
+
     </div>
   );
 }
---
+++ src/FirebaseConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyA3lvXW5JzXWlNGciXX_P3i2t3p4Ig9h8w",
+    authDomain: "my-react-app001.firebaseapp.com",
+    projectId: "my-react-app001",
+    storageBucket: "my-react-app001.appspot.com",
+    messagingSenderId: "451183661128",
+    appId: "1:451183661128:web:2cc0735932061350b6c87d",
+    measurementId: "G-0YJZNSJ751"
+
+};
+
+export default firebaseConfig;
